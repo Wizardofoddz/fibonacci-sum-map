57f68e941d235c725fb828b31093579641938d19
Roy van de Water <roy.vwater@gmail.com>
2017-12-20 12:46:43 -0700

    v1.0.0 Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fibonacci-sum-map
+
 Calculate the Nth digit of the Fibonacci Sequence
+
+For example:
+
+```shell
+echo 1 | fibonacci-sum-map
+#> 1
+
+echo 7 | fibonacci-sum-map
+#> 13
+```
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	inputBytes, err := ioutil.ReadAll(bufio.NewReader(os.Stdin))
+	if err != nil {
+		log.Panicln("Error reading stdin:", err.Error())
+	}
+
+	input := strings.Trim(string(inputBytes), "\n\t ")
+	n, err := strconv.ParseUint(string(input), 10, 64)
+	if err != nil {
+		log.Panicln("Error parsing stdin as uint64:", err.Error())
+	}
+
+	fmt.Println(Fibonacci(n))
+}
+
+// Fibonacci computes the nth digit of the Fibonacci Sequence
+func Fibonacci(n uint64) uint64 {
+	sequence := []uint64{0, 1}
+
+	if n < 2 {
+		return sequence[n]
+	}
+
+	for i := uint64(2); i <= n; i++ {
+		lastValue := sequence[len(sequence)-1]
+		secondToLastValue := sequence[len(sequence)-2]
+
+		sequence = append(sequence, lastValue+secondToLastValue)
+	}
+
+	return sequence[len(sequence)-1]
+}
